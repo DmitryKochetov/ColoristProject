7635129aec77912f4fe92310c101c513ae8539a8
Kochetov Dmitry <kochetov.dmitry@mail.ru>
2024-02-13 12:59:07 +0300

    some changes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import {createHead} from '@vueuse/head'
+import VueYandexMetrika from 'vue3-yandex-metrika'
 
 const head = createHead()
 
-createApp(App).use(head).use(store).use(router).mount('#app')
+createApp(App).use(head).use(store).use(router).use(VueYandexMetrika, {
+	id: 96397865,
+	router: router,
+	env: process.env.NODE_ENV
+    // other options
+}).mount('#app')
